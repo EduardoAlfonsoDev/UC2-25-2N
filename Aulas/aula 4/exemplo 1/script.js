4d08505133103c36022cdf4169e6c5d3bca56def
EduardoAlfonsoDev <eduardoalfonsodev@gmail.com>
2025-09-22 21:59:37 -0300

    adicionado o exercicio

---
+++ Aulas/aula 4/exemplo 1/script.js
@@ -0,0 +1,15 @@
+/* Comentário de um texto  */
+
+// comentário de 1 linha
+
+/*
+console.log ("Hello World class 25-2N")
+console.log ("Hello world again")
+
+prompt("What's your name?")
+*/
+
+const nome = "Eduardo"
+const idade = 23
+
+console.log("o nome é:", nome, "e sua idade é:", idade)
---
+++ Aulas/aula 4/exercicios/script.js
@@ -0,0 +1,9 @@
+const nome = "Eduardo"
+const sobrenome = "Alfonso"
+const idade = 23
+let estuda = true
+let naoEstuda = false
+
+let estudante = (estuda == true) || (naoEstuda == false)
+
+console.log( nome, sobrenome, idade, "é estudante?:", estuda)
