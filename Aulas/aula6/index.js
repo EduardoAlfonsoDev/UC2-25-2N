fa390b1fac919f759afa765ab80873ede8ed942f
EduardoAlfonsoDev <eduardoalfonsodev@gmail.com>
2025-09-24 21:51:31 -0300

    novos exercicios

---
+++ Aulas/aula6/index.js
@@ -0,0 +1,44 @@
+/*
+// let é uma variavel que você pode alterar com o tempo
+let a = 10 // definiu a variavel e seu valor
+let b = 10 // definiu a variavel e seu valor
+
+console.log(b) // output da varialvel b
+
+b = 5 // alterou a varialvel
+
+console.log(a,b) // output das variaveis a e b
+
+
+let a2 = 10 // definiu a variavel e seu valor
+let b2 = 20 // definiu a variavel e seu valor
+
+c = a2 // alterou a variavel c com o valor igual a variavel a2
+b2 = c // alterou a variavel b2 com o valor igual a variavel c
+a2 = b2 // alterou  a variavel a2 com o valor igual a variavel b2
+
+console.log(a2, b2, c) // output das variaveis a, b e c
+
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Qauanto você recebe por dia?")
+console.log("Você recebe", t, "/", p, "por hora")
+let horasTrabalhadas = prompt("Quantas horas você trabalha por dia?")
+let recebePorDias = prompt("Quanto você recebe por dia?")
+console.log("Você recebe", recebePorDias, "/", horasTrabalhadas, "por hora")
+*/
+
+let nome 
+console.log(nome)
+let idade
+console.log(idade)
+console.log(typeof(nome, idade))
+// undefined pq está vazio
+
+prompt("Qual seu nome?", eduardo)
+nome = eduardo
+
+prompt("Qual sua idade?", 23)
+idade = 23
+console.log(eduardo, 23)
+
+
