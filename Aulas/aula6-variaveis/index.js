3f612c48395ee8140ca58a720474174cfe0b0128
EduardoAlfonsoDev <eduardoalfonsodev@gmail.com>
2025-09-29 21:14:48 -0300

    novos exercicios e exemplos

--- Aulas/aula6-variaveis/index.js
+++ Aulas/aula6-variaveis/index.js
@@ -13,7 +13,7 @@ console.log(a,b) // output das variaveis a e b
 let a2 = 10 // definiu a variavel e seu valor
 let b2 = 20 // definiu a variavel e seu valor
 
-c = a2 // alterou a variavel c com o valor igual a variavel a2
+c = a2 // criou a variavel c com o valor igual a variavel a2
 b2 = c // alterou a variavel b2 com o valor igual a variavel c
 a2 = b2 // alterou  a variavel a2 com o valor igual a variavel b2
 
@@ -25,7 +25,6 @@ console.log("Você recebe", t, "/", p, "por hora")
 let horasTrabalhadas = prompt("Quantas horas você trabalha por dia?")
 let recebePorDias = prompt("Quanto você recebe por dia?")
 console.log("Você recebe", recebePorDias, "/", horasTrabalhadas, "por hora")
-*/
 
 let nome 
 console.log(nome)
@@ -42,3 +41,52 @@ prompt("Qual sua idade?", idade)
 console.log("Qual seu nome?", nome, "Qual sua idade?", idade)
 
 
+let roupa = prompt("Você está usando roupa preta hoje?")
+
+console.log(roupa)
+
+let tattoo = prompt("Você tem celular?")
+
+console.log(tattoo)
+
+let sapato = prompt("Você está usando sapato fechado hoje?")
+
+console.log(sapato)
+
+console.log("Você está usando roupa preta hoje?", roupa, "Você tem celular?", tattoo, "Você está usando sapato fechado hoje?", sapato )
+
+
+
+let roupa = "sim"
+//console.log(roupa)
+let tattoo = "sim"
+//console.log(tattoo)
+let sapato = "sim"
+//console.log(sapato)
+
+console.log("Você está usando roupa preta hoje?", roupa, "Você tem celular?", tattoo, "Você está usando sapato fechado hoje?", sapato )
+
+
+/*
+let a = 10
+let b = 25
+
+c = a
+a = b
+b = c
+
+
+console.log("O novo valor de a é",a)
+console.log("O novo valor de b é",b)
+
+*/
+
+/*
+    //desafio 
+let numero1 = prompt("")
+let numero2 = prompt("")
+
+let x = numero1 + numero2
+
+let y = numero1 * numero2
+*/
