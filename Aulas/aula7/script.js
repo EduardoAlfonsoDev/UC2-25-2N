8390d9a29824268bc1a32dbf7f03a286f7c4d8c6
EduardoAlfonsoDev <eduardoalfonsodev@gmail.com>
2025-09-29 21:51:03 -0300

    adicionando aula sete

---
+++ Aulas/aula7/script.js
@@ -0,0 +1,39 @@
+// cria primeira variavel com o valor de 10
+const primeiroNumero = 10
+
+// cria a segunda variavel com o valor de 20
+const segundoNumero = 20 
+
+// cria a variavel resultado para a soma da expressão
+//const resultado = (primeiroNumero + segundoNumero + 4)
+
+// cria a variavel resultado para a subtração
+const resultado = primeiroNumero - segundoNumero
+
+
+// imprime o valor resultante
+console.log(resultado)
+
+const primeiroValor = 10
+const segundoValor = 25
+
+// cria a variavel novoResultado para a multiplicação
+//const novoResultado = primeiroValor * segundoValor
+
+// cria a variavel novoResultado para a divisão
+const novoResultado = primeiroValor / segundoValor
+console.log(novoResultado)
+
+let minhaidade = 23
+minhaidade = minhaidade + 1
+console.log(minhaidade)
+
+//   simplificado
+let resultadoSimples = 100
+
+ resultadoSimples += 50
+console.log(resultadoSimples)
+
+
+
+
