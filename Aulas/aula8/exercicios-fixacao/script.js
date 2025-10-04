3a99a2b6a8a19a95e07b311ce2e172dc1272972f
EduardoAlfonsoDev <eduardoalfonsodev@gmail.com>
2025-10-03 21:44:19 -0300

    aula 8

--- Aulas/aula8/exercicios-fixacao/script.js
+++ Aulas/aula8/exercicios-fixacao/script.js
@@ -26,7 +26,12 @@ console.log(soma)
 
 // porque o prompt puxa como string então tem que transformar para number
 
-*/
+*/ 
+
+
+/*
+
+// exer 3
 
 let idade = prompt('qual sua idade')
 const idade2 = Number(idade)
@@ -35,5 +40,59 @@ const idadeAmigo2 = Number(idadeAmigo)
 
 
 console.log('Sua idade é maior do que seu melhor amigo?', idade2 >= idadeAmigo2)
-console.log('diferença de idade', idade2 !== idadeAmigo2)
+console.log('diferença de idade', idade2 - idadeAmigo2 )
+
+*/
+
+// exer 4
+
+/*
+
+let numeroPar = prompt('Me de um número par')
+
+console.log(numeroPar % 2) // todos número par fica 0 
+// e todo número ímpar fica 1
+
+
+*/
+
+/*
+
+// exer 5
+
+let anos = prompt('Me diga sua idade')
+let anosN = Number(anos)
+console.log(anos)
+console.log(anos * 12)
+console.log(anos * 365)
+console.log(anos * 24)
+
+*/
+
+// exer 6
+ /*
+let numeroUm = prompt('Me de um número')
+let numeroUmN = Number(numeroUm)
+let numeroDois = prompt('Me de outro número')
+let numeroDoisN = Number(numeroDois)
+let restoDaDivisao = numeroUmN % numeroDoisN
+let restoDaDivisao2 = numeroDoisN % numeroUmN
+
+console.log(restoDaDivisao, restoDaDivisao2)
+
+console.log('O primeiro numero é maior que segundo?', numeroUmN > numeroDoisN)
+console.log('O primeiro numero é igual ao segundo?', numeroUmN === numeroDoisN)
+
+
+console.log('O primeiro numero é divisível pelo segundo?', restoDaDivisao >= 0)
+console.log('O segundo numero é divisível pelo primeiro?', restoDaDivisao2 >= 0)
+
+*/
+
+// exer 7
+
+
+                
+
+
 
