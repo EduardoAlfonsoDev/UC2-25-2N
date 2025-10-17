17a4178044ee35881b3a65ca60ddfcb3a3d5ec4f
EduardoAlfonsoDev <eduardoalfonsodev@gmail.com>
2025-10-17 20:40:27 -0300

    aula 11 exercicios

--- Aulas/aula11/exercicios/script.js
+++ Aulas/aula11/exercicios/script.js
@@ -1,3 +1,4 @@
+/*
 const respostasDoUsuario = prompt('digite o número que você quer testar')
 const numero = Number(respostasDoUsuario)
 
@@ -11,6 +12,8 @@ else {
 // b- Números pares
 // c- Números ímpares
 
+*/
+/*
 // exercício 2
 let fruta = prompt('Escolha uma fruta')
 let preco
@@ -32,8 +35,62 @@ switch (fruta) {
         break
 }
 console.log('o preço da fruta ', fruta, ' e ', 'R$ ', preco)
+*/
+//
+//
+//
+/*
+const numero = Number(prompt('Digite o primeiro número.'))
+if(numero>0){
+    console.log('Esse número passou no teste')
+    let mensagem = 'essa mensagem é secreta!!!'
+}
+//
+// o erro é criar uma variavel dentro do escopo, para corrigir é só criar a variavel em baixo da const nuemro
 
-//
-//
-//
-//
+*/
+/*
+ // exerc code 1
+ const idade = Number(prompt('Qual sua idade?'))
+ 
+ let idade1 = idade > 18
+ if(idade > 18){
+    console.log('Voce pode dirigir')}
+else{
+    console.log('Voce não pode dirigir')
+ }
+*/
+ // exerc code 2
+
+let turno = prompt('Digite M (matutino) ou V (Vespertino) ou N (Noturno).')
+turno = turno.toLowerCase()
+//const resposta1 = 'm'
+//const resposta2 = 'v'
+// const respposta3 = 'n'
+if(turno === 'm'){
+    console.log('Bom dia!')}
+else if(turno === 'v'){
+    console.log('Boa tarde!')}
+else if (turno === 'n'){
+    console.log('Boa noite!')
+}
+else 
+    console.log('turno não encontrado')
+
+switch(turno){
+    case 'm':
+        console.log('Bom dia!')
+        break
+    case 'v':
+        console.log('Boa tarde!')
+        break
+    case 'n':
+        console.log('Boa noite!')
+        break
+    default:
+        console.log('turno não encontrado')
+}
+
+
+
+
