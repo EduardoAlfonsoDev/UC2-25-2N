2bedd7a0819228d99a9860f943301aaaf8e24c23
EduardoAlfonsoDev <eduardoalfonsodev@gmail.com>
2025-11-07 21:42:29 -0300

    aula nova

--- Aulas/aula16/exercicios/script.js
+++ Aulas/aula16/exercicios/script.js
@@ -1,7 +1,9 @@
+/*
 let soma = function () {
   return 2 + 2;
 };
 console.log(`A soma é ${soma(2, 2)}`);
+*/
 /*
 const soma = function(){
     const num1 = 5
@@ -12,12 +14,12 @@ const soma = function(){
 }
 soma()
 */
-
+/*
 let dobro = () => {
   return 5 * 2;
 };
 console.log(`o dobro será ${dobro(5, 2)}`);
-
+*/
 /*
 const dobro = () => {
     let resultadoDobro = 5 * 2
@@ -26,24 +28,84 @@ const dobro = () => {
 dobro();
 */
 
+/*
 const determinaMonstro = (
   temPeleGelada,
   transformaComLuaCheia,
   brilhaAoSol,
   temSedeDeSangue
 ) => {
-    vampiro = temPeleGelada 
-    lobisomen = transformaComLuaCheia
-    vampiro = brilhaAoSol
-    vampiro= temSedeDeSangue 
+  
   if (temPeleGelada && brilhaAoSol && temSedeDeSangue) {
-    console.log("voce é um vampiro");
+    return 'voce é um vampiro';
   } else if (transformaComLuaCheia) {
-    console.log("voce é um lobisomen");
+    return 'voce é um lobisomen'
   }
   else
-  console.log('voce é um humano')
+    return 'voce é um humano'
 
-return 
+ 
 };
-console.log(determinaMonstro);
+
+console.log(determinaMonstro(true,false,true,true));
+console.log(determinaMonstro(false,true,false,false));
+console.log(determinaMonstro(false,false,false,false));
+*/
+
+// exercícios de fixação
+// 1
+
+/*
+function minhaFuncao(variavel){
+  return variavel * 5
+}
+
+console.log(minhaFuncao(2))
+console.log(minhaFuncao(10))
+
+// a) vai ser impresso uma multiplicação
+// b) vai dar erro, pois ele precisa de um console para invocar, ou ter um console dentro da funçãosuario
+*/
+
+/*
+// 2
+let textoDoUsuario = prompt('Insira um texto')
+
+const outraFuncao = function(texto){
+  return texto.toLowerCase().includes('cenoura')
+}
+const reposta = outraFuncao(textoDoUsuario)
+console.log(reposta)
+
+//a)
+//b)
+*/
+// 3
+
+let mensagem = () => {
+   return 'eu sou eduardo, tenho 23 anos, moro em são leopoldo e sou estudante'
+}
+console.log(mensagem())
+
+// 4
+
+let soma = (num1, num2) =>{
+  return num1 + num2
+}
+console.log(soma(5,5))
+
+
+let booleano = (num1, num2) => {
+  return num1 >= num2
+}
+console.log(booleano(3,5))
+
+let par = (num1, num2) => {
+  return num1 >= num2 %2
+}
+console.log(par(2,3))
+
+//let texto = ('oi') => {
+// return texto.length.toUpperCase()
+//}
+// console.log(texto)
